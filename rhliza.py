d8c1e28038c5271142125e9c3b01c8a630ef553b
Emile Snyder <emile.snyder@gmail.com>
2014-04-02 21:54:00 -0700

    Try to get working again.

--- rhliza.py
+++ rhliza.py
@@ -1,13 +1,20 @@
 #!/usr/bin/env python
-
+# DON'T apt-get install python-twitter
+# using https://pypi.python.org/pypi/twitter
 import os, sys, re, time
+#import twitter as tw
 from twitter import *
 from nltk.chat.eliza import eliza_chatbot as bot
 
 # Set these in your .bashrc or something.
 # Find them on the twitter dev web site for the '@rhlize' user, weak password 't*'
-t = Twitter(auth=OAuth(os.getenv('RHLIZA_ACCESS_TOKEN'), os.getenv('RHLIZA_ACCESS_TOKEN_SECRET'), 
-                       os.getenv('RHLIZA_CONSUMER_KEY'), os.getenv('RHLIZA_CONSUMER_SECRET')))
+at =os.getenv('RHLIZA_ACCESS_TOKEN')
+ats=os.getenv('RHLIZA_ACCESS_TOKEN_SECRET')
+ck =os.getenv('RHLIZA_CONSUMER_KEY')
+cs =os.getenv('RHLIZA_CONSUMER_SECRET')
+
+#print at, ats, ck, cs
+t = Twitter(auth=OAuth(at, ats, ck, cs))
 
 # map of userid -> tweetid
 # of the last message we have responded to from any user
@@ -18,19 +25,27 @@ lastmsgs = dict()
 
 while True:
     print "getting user_timeline"
-    x = t.statuses.mentions()
+    #x = t.statuses.mentions()
+    #x = t.statuses.home_timeline()
+    x = t.search.tweets(q="@roguehacklab")
     
     print "got response x, len(x):", len(x), ", type(x):", type(x)
-    print x[0].keys()
-    print
-    print dir(bot)
+    print dir(x)
+    #print x
+    #print x[0].keys()
+    for r in x.values():
+        print x.keys()
+        for s in x['statuses']:
+            print s['text']
+    #print
+    #print dir(bot)
     print
 
-    print x[0]['user']['screen_name'], x[0]['text']
+    #print x[0]['user']['screen_name'], x[0]['text']
     # The first 'tweet' in the timeline
     # x[0]
 
-    print "reply with: ", bot.respond(x[0]['text'])
+    #print "reply with: ", bot.respond(x[0]['text'])
     break # don't actually keep doing this right now
 
     time.sleep(60)
